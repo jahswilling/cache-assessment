396de476449b34062a7b0e33ad295aac63bded76
jahswilling <jahswilling@gmail.com>
2025-04-21 03:49:34 +0100

    update ci/cd

--- cache_app_demo/cache_app_demo/settings.py
+++ cache_app_demo/cache_app_demo/settings.py
@@ -80,23 +80,23 @@ WSGI_APPLICATION = "cache_app_demo.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": os.getenv('DATABASE_ENGINE', 'django.db.backends.postgresql'),
-        "NAME": os.getenv('DATABASE_NAME'),
-        "USER": os.getenv('DATABASE_USER'),
-        "PASSWORD": os.getenv('DATABASE_PASSWORD'),
-        "HOST": os.getenv('DATABASE_HOST'),
-        "PORT": os.getenv('DATABASE_PORT', '5432'),
-    }
-}
-
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
+#         "ENGINE": os.getenv('DATABASE_ENGINE', 'django.db.backends.postgresql'),
+#         "NAME": os.getenv('DATABASE_NAME'),
+#         "USER": os.getenv('DATABASE_USER'),
+#         "PASSWORD": os.getenv('DATABASE_PASSWORD'),
+#         "HOST": os.getenv('DATABASE_HOST'),
+#         "PORT": os.getenv('DATABASE_PORT', '5432'),
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
